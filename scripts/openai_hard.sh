4b101d965af0d237fd7eaf79641749133964301d
Xuanlin (Simon) Li <35481101+xuanlinli17@users.noreply.github.com>
2023-11-03 20:57:02 -0700

    Update openai_hard.sh

--- scripts/openai_hard.sh
+++ scripts/openai_hard.sh
@@ -4,5 +4,5 @@ python hlm/app/evaluate_qa.py --dataset MATH --dataset-args="dict(levels=5, incl
 # Question retrieval
 python hlm/app/evaluate_qa.py --dataset MATH --dataset-args="dict(levels=5, include_asy=False)" --dataset-name=math_hard --reasoning-fn explore --reasoning-args="dict(prompt='dynamic_1shot.txt', type='retrieval-question', retrieval_ratio={'question': 1}, n_retrievals=4, temperature=0.7, n=16, demo_kwargs=dict(dataset_name='MATH', include_asy=False, split='train'))"
 
-# Proposed Ideas
+# High-level hints and tactics
 python hlm/app/evaluate_qa.py --dataset MATH --dataset-args="dict(levels=5, include_asy=False, aug='/data/Projects/AGI/projects/exploration/results/gpt4_answer_techniques.json')" --dataset-name=math_hard --reasoning-fn explore --reasoning-args="dict(prompt='use_techniques.txt', type='key-techniques', n=16, n_retrievals=4)"
